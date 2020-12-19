d153899479d6b7fe0a603637f41e19270c929dc2
unknown <adityaanantwar123@gmail.com>
2020-12-19 15:21:25 +0530

    commit before merge

--- genio-replicate/src/index.js
+++ genio-replicate/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Footer from './footer.js';
+import Gplay from './gplay';
+import Improve from './improve'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Improve />
+  <Gplay />
+  <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );
