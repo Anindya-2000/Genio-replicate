238e6c1cf6c6da33c2d71663d9587fd068dcdda8
unknown <adityaanantwar123@gmail.com>
2020-12-21 15:55:58 +0530

    some corrections

--- genio-replicate/src/gplay.js
+++ genio-replicate/src/gplay.js
@@ -7,7 +7,7 @@ export default function gplay(){
         <Grid container spacing={0}>
             <Container>
                 <Grid item xs={12} >
-                    <Typography variant="h3" align="center" className="gplay-text" >Start your kid's journey.</Typography>
+                    <p className="gplay-text">Start your kid's journey</p>"
                 </Grid>
                 <Grid item xs={12} className="gplay-container">
                     <img src="img/google_play_logo.png" alt="" className="gplay"/>
