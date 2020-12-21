1b6e092b36a1b6faf3f01f3ee82d4407a370bd6c
Anindya Sikdar <58126719+Anindya-2000@users.noreply.github.com>
2020-12-22 01:21:45 +0530

    Drawer issues resolved

--- genio-replicate/src/Header.js
+++ genio-replicate/src/Header.js
@@ -3,9 +3,8 @@ import Grid from '@material-ui/core/Grid';
 import {makeStyles} from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Box from "@material-ui/core/Box";
-import Drawer from './Drawer.js';
 import './Drawer.css';
-import {useState,setState} from 'react';
+import {useState,setState,useEffect} from 'react';
 
 const useStyles = makeStyles((theme) => ({
     root:{
@@ -64,7 +63,17 @@ const useStyles = makeStyles((theme) => ({
 
 const Header = () => {
     const classes = useStyles();
-    const [isOpen,setIsOpen] = useState(false);
+    let isOpen = false;
+
+    useEffect(()=>{
+        window.addEventListener('click',(ev)=>{
+            if(!ev.target.matches('.DontClose')&&!ev.target.matches('.bars')){
+                console.log(isOpen,ev.target)
+                if(isOpen)
+                    rotateDivs();
+            }
+        })
+    },[1]);
 
     const DropDown = (val) =>{
         const DD = document.getElementsByClassName('headerMenu__wrapper')[0];
@@ -74,6 +83,26 @@ const Header = () => {
         else{
             DD.classList.remove('DD_animation');
         }
+    }
+
+    const rotateDivs = () =>{
+        const div1 = document.getElementsByClassName("stick-1")[0];
+        const div2 = document.getElementsByClassName("stick-2")[0];
+        const div3 = document.getElementsByClassName("stick-3")[0];
+        if(isOpen){
+            isOpen = false;
+            div2.style.display = "block";
+            div1.classList.remove('rotate-cc');
+            div3.classList.remove('rotate-c');
+            DropDown(0);
+        }
+        else{
+            isOpen = true;
+            div2.style.display = "none";
+            div1.classList.add('rotate-cc');
+            div3.classList.add('rotate-c');
+            DropDown(1);
+        }     
     }
 
     return (
@@ -90,15 +119,23 @@ const Header = () => {
                 </a>
             </Box>
 
-            <Box component={Grid} item display = {{lg:"none",md:"none",sm:"none"}} className = {classes.Drawer}><Drawer isOpen = {isOpen} setIsOpen = {setIsOpen} DropDown = {DropDown}/></Box>
+            <Box component={Grid} item display = {{lg:"none",md:"none",sm:"none"}} className = {classes.Drawer}>
+                <a href="javascript:void(0)" style = {{"position":"relative"}} className = "bars">
+                    <div onClick = {()=>rotateDivs()} className = "container bars">
+                        <div className = "stick stick-1 bars"></div>
+                        <div className = "stick stick-2 bars"></div>
+                        <div className = "stick stick-3 bars"></div>
+                    </div>
+                </a>
+            </Box>
     
-            <div className = "headerMenu__wrapper" style = {{"background-color":"#ffffff"}}>
-                <ul className = "headerMenu__links">
+            <div className = "headerMenu__wrapper DontClose" style = {{"background-color":"#ffffff"}}>
+                <ul className = "headerMenu__links DontClose">
                     <li>
-                        <a className = "drawerLink" href = "#">
+                        <a className = "drawerLink DontClose" href="#">
                             Download App
-                            <svg width = "13" height = "12" xmlns = "http://www.w3.org/2000/svg">
-                                <path d = "M9.557 7H1a1 1 0 1 1 0-2h8.586L7.007 2.421a1 1 0 0 1 1.414-1.414l4.243 4.243c.203.202.3.47.292.736a.997.997 0 0 1-.292.735L8.42 10.964A1 1 0 1 1 7.007 9.55L9.557 7z" fill = "#51a3ff"></path>
+                            <svg width = "13" height = "12" xmlns = "http://www.w3.org/2000/svg" className = "DontClose">
+                                <path className = "DontClose" d = "M9.557 7H1a1 1 0 1 1 0-2h8.586L7.007 2.421a1 1 0 0 1 1.414-1.414l4.243 4.243c.203.202.3.47.292.736a.997.997 0 0 1-.292.735L8.42 10.964A1 1 0 1 1 7.007 9.55L9.557 7z" fill = "#51a3ff"></path>
                             </svg>
                         </a>
                     </li>
