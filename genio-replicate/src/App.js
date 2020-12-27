3c1d1aa92cc02077e00485f7b3cbd9c78585b2bb
Anindya Sikdar <58126719+Anindya-2000@users.noreply.github.com>
2020-12-27 20:33:26 +0530

    imports fixed

--- genio-replicate/src/App.js
+++ genio-replicate/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import loadable from '@loadable/component'
+import Gplay from './gplay.js'
+import Footer from './footer.js'
+
 const Header = loadable(() => import('./Header'));
 const MainUpper = loadable(() => import('./MainUpper'));
+
 const App = () => {
   return (
     <div>
       <Header />
       <MainUpper />
       <Gplay />
-    `<Footer />
+    ` <Footer />
     </div>
   )
 };
