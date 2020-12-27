0a65b0c9722adafdc87c845b6571a3868b99d8b1
Bhargavamacha <mbsai29@gmail.com>
2020-12-27 21:10:05 +0530

    fixed merge conflict

--- genio-replicate/src/App.js
+++ genio-replicate/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import loadable from '@loadable/component'
-const MainUpper = loadable(() => import('./MainUpper'));
+const MainUpper = loadable(() => import('./Body'));
 const Footer = loadable(() => import('./footer'));
 const Gplay = loadable(() => import('./gplay'));
 const App = () => {
